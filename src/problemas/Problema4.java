16dc6b885943f997f939eec373c71ef03d237399
Alfredo <bryanmr55@gmail.com>
2021-01-16 19:01:08 -0600

    Se agrego problema 4 resuelto

--- src/problemas/Problema4.java
+++ src/problemas/Problema4.java
@@ -12,16 +12,21 @@ La suma es 35 y se sumaron 5 términos
 public class Problema4 {
 
     public static void main(String[] args) {
-        int valorInicial, valorFinal, contador = 0;
+        int sumatoria = 0, valorInicial, valorFinal, contador = 0;
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Ingresa el valor inicial: ");
         valorInicial = sc.nextInt();
         System.out.println("Ingrese el valor final: ");
         valorFinal = sc.nextInt();
-        for (int i = valorInicial; i <= valorFinal; i++) {
-            valorInicial++;
+        
+        int minimo = Math.min(valorInicial, valorFinal);
+        int maximo = Math.max(valorInicial, valorFinal);
+        
+        for (int i = minimo; i <= maximo; i++) {
+            sumatoria +=i;
             contador++;
         }
-        System.out.println("La suma es: " + valorInicial + " y se sumaron " + contador + " terminos");
+        System.out.println("La suma es: " + sumatoria + " y se sumaron " + contador + " términos");
     }
 }
