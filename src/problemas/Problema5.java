ac580aa8a510a9665434d8c770365c28d806d342
Alfredo <bryanmr55@gmail.com>
2021-01-16 19:49:08 -0600

    Terminado problema 5

--- src/problemas/Problema5.java
+++ src/problemas/Problema5.java
@@ -4,14 +4,19 @@ import java.util.Random;
 
 /*
 5) Desarrollar un programa que obtenga 100 números de manera aleatoria en un rango de 0 a 100 y contar cuántos de ellos son impares.
-Aa
-*/
+ */
 public class Problema5 {
+
     public static void main(String[] args) {
-        int valores;
-        Random rd = new Random();
-       valores = (int)(rd.nextDouble() * 10 + 0);
-        System.out.println("valores: " + valores);
-//        (int)(rnd.nextDouble() * 10 + 0);
+        int[] valores = new int[100];
+        int contadorImpares=0;
+        Random rnd = new Random();
+        for (int i = 0; i < valores.length; i++) {
+            valores[i] = (int) (rnd.nextDouble() * 100);
+            if(valores[i]%2!=0){
+                contadorImpares++;
+            }
+        }
+        System.out.println("La cantidad de números impares son: " + contadorImpares);
     }
 }
